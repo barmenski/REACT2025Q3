36514db2fe1e3ebc5f656ae1de80d1ae56e9de6f
barmenski <1kiloom@gmail.com>
2025-07-18 00:17:20 +0300

    init: task#2 start

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/REACT2025Q3/',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setup.ts',
+    coverage: {
+      provider: 'v8',
+      include: ['src//*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src//.test.{js,jsx,ts,tsx}',
+        'src/**/.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/*/.d.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
 });
